1a0172bf5997c94eb9b4c925e879181655edf262
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-03-30 00:36:31 +0900

    JS: slide

--- js/image.js
+++ js/image.js
@@ -1,11 +1,16 @@
 (() => {
-  const headerImg = document.querySelector(".header .card .img");
-  const images = [
-    "NGCKOREA (2)",
-    "NGCKOREA (4)",
-    "NGCKOREA (5)",
-    "NGCKOREA (6)",
-    "NGCKOREA (7)",
-  ];
+  const prev = document.querySelector(".control .prev");
+  const next = document.querySelector(".control .next");
+  const card = document.querySelector(".header .card");
+  const size = document.querySelector(".header .img").clientWidth;
+  const itemElems = document.querySelectorAll(".header .item");
+  console.log(size);
   let current = 0;
+
+  next.addEventListener("click", function () {
+    console.log("clicked");
+    current++;
+    card.style.transform = `translateX(${current * -size}px)`;
+    card.style.transition = "0.5s";
+  });
 })();
