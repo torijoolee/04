f708d02ce35975b96811c121d62ec98b3cf49b61
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-03-29 17:35:41 +0900

    HTML,CSS:active-bar

---
+++ js/image.js
@@ -0,0 +1,11 @@
+(() => {
+  const headerImg = document.querySelector(".header .card .img");
+  const images = [
+    "NGCKOREA (2)",
+    "NGCKOREA (4)",
+    "NGCKOREA (5)",
+    "NGCKOREA (6)",
+    "NGCKOREA (7)",
+  ];
+  let current = 0;
+})();
